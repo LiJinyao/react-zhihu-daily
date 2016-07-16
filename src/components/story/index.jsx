1cd25fc0ee57d2e7b565348e65fcf4faa4172451
LiJinyao <lijinyao@outlook.com>
2016-07-16 17:05:59 +0800

    替换img的src链接

--- src/components/story/index.jsx
+++ src/components/story/index.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { zhihuAPI } from '../../statics';
+
+// 把文章body中img的src替换成自己服务器的连接，绕过盗链处理。
+function replaceImgSrcToProxy(htmlString) {
+  const element = document.createElement('div');
+  element.innerHTML = htmlString;
+  for (const child of element.getElementsByTagName('img')) {
+    const imgUrl = child.getAttribute('src');
+    child.setAttribute('src', `${zhihuAPI}${imgUrl}`);
+  }
+  return element;
+}
 
 const Story = ({ story }) => (
-  <div dangerouslySetInnerHTML={{ __html: story.body }}></div>
+  <div dangerouslySetInnerHTML={{ __html: replaceImgSrcToProxy(story.body).innerHTML }}>
+  </div>
 );
+
+Story.propTypes = {
+  story: PropTypes.object,
+};
 
 export default Story;
