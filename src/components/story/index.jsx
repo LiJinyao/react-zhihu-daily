100be6d5685994ea9da81a1846df06e0a4fb8d49
LiJinyao <lijinyao@outlook.com>
2016-07-24 19:43:43 +0800

    adjust story style

--- src/components/story/index.jsx
+++ src/components/story/index.jsx
@@ -1,24 +1,29 @@
 import React, { PropTypes } from 'react';
 import { zhihuAPI } from '../../statics';
-
+import style from './story.styl';
 // 把文章body中img的src替换成自己服务器的连接，绕过盗链处理。
-function replaceImgSrcToProxy(htmlString, styleUrl) {
+function replaceImgSrcToProxy(htmlString, styleUrl, headerImgUrl) {
   const element = document.createElement('div');
   // 使用zhihu的样式
-  const style = document.createElement('link');
-  style.setAttribute('rel', 'stylesheet');
-  style.setAttribute('href', styleUrl);
-  element.innerHTML = style.outerHTML + htmlString;
+  const zhihuStyle = document.createElement('link');
+  zhihuStyle.setAttribute('rel', 'stylesheet');
+  zhihuStyle.setAttribute('href', styleUrl);
+  element.innerHTML = zhihuStyle.outerHTML + htmlString;
   for (const child of element.getElementsByTagName('img')) {
     const imgUrl = child.getAttribute('src');
     child.setAttribute('src', `${zhihuAPI}${imgUrl}`);
   }
+  const headerImg = element.querySelector('.img-place-holder');
+  headerImg.innerHTML = `<img src="${zhihuAPI}${headerImgUrl}" alt="header" />`;
   return element;
 }
 
 const Story = ({ story }) => (
   <div
-    dangerouslySetInnerHTML={{ __html: replaceImgSrcToProxy(story.body, story.css[0]).innerHTML }}
+    className={style.storyBody}
+    dangerouslySetInnerHTML={{
+      __html: replaceImgSrcToProxy(story.body, story.css[0], story.image).innerHTML,
+    }}
   >
   </div>
 );
