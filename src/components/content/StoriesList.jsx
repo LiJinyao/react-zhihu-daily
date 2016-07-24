5fc490e10dab1424a4b93e6d6aab2572219ebf4b
LiJinyao <lijinyao@outlook.com>
2016-07-24 14:05:21 +0800

    fix stylus

--- src/components/content/StoriesList.jsx
+++ src/components/content/StoriesList.jsx
@@ -1,6 +1,13 @@
 import React, { PropTypes } from 'react';
 import style from './StoriesList.styl';
 import StoryItem from './StoryItem';
+
+function getDate(dateString) {
+  const month = parseInt(dateString.substring(4, 6), 10);
+  const day = parseInt(dateString.substring(6, 8), 10);
+  return `${month}月${day}日`;
+}
+
 const StoriesList = ({ stories }) => (
   <div className={style.storyList}>
     {
@@ -13,15 +20,19 @@ const StoriesList = ({ stories }) => (
           )
         );
         // push date tag in the front
-        storyList.unshift(<div className={style.date}>{dailyStory.date}</div>);
+        storyList.unshift(<div className={style.date}>{getDate(dailyStory.date)}</div>);
 
         // push next day in the end
-        storyList.push(<input className={style.nextDay} type="button" value={"Next Day"} />);
+        storyList.push(<span className={style.nextDay}>
+          <input type="button" value={"Next Day"} onClick={() => { console.log("Hello") }} />
+        </span>);
         return storyList;
       })
     }
   </div>
 );
+
+
 
 StoriesList.propTypes = {
   stories: PropTypes.array.isRequired,
