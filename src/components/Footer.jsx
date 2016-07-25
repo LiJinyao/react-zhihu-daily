74a15403167c7c25d6159b75d2ebc0f6831a0fb3
LiJinyao <lijinyao@outlook.com>
2016-07-25 17:27:36 +0800

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import style from './Footer.styl';
 
-const Footer = () => ((<div className={style.footer}></div>));
+const Footer = () => (
+  <div className={style.footerContainer}>
+    <div className={style.footer}>
+      <span className={style.intro}>用react实现的zhihu daily网页版</span>
+    </div>
+  </div>
+  );
 
 export default Footer;
