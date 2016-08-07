9136aa1167422634a200b71d550189b5596502f7
LiJinyao <lijinyao@outlook.com>
2016-08-07 23:10:10 +0800

    production conf

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -49,7 +49,8 @@ module.exports = {
     },
     {
       test: /\.svg$/,
-      loader: 'url-loader?limit=0&name=images/[name].[ext]'
+      loader: 'url-loader?limit=10000&name=images/[name].[ext]',
+      exclude: path.join(__dirname, 'src', 'statics', 'iconfont')
     },
     {
       test: /\.(woff2?|otf|eot|svg|ttf)$/,
