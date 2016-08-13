3c817657f77f912f0a2cb5dce9640bf67aff87ac
LiJinyao <lijinyao@outlook.com>
2016-08-13 23:50:49 +0800

    don't create

--- server/database/createTable.js
+++ server/database/createTable.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
 connection.connect();
 
 function createTable() {
-  connection.query('CREATE TABLE IF NOT EXISTS stories (id INT UNSIGNED ZEROFILL)');
+  // connection.query('CREATE TABLE IF NOT EXISTS stories (id INT UNSIGNED ZEROFILL)');
 }
 
 connection.query('SHOW TABLES LIKE \'daily\'', (err, rows) => {
