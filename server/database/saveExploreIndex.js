8090002658dc47b526fce1ccd76a7de020c457d0
LiJinyao <lijinyao@outlook.com>
2016-08-19 10:45:46 +0800

    delete helper

--- server/database/saveExploreIndex.js
+++
@@ -1,9 +0,0 @@
-import query from './sqlHelper';
-
-export default function (item) {
-  return new Promise((resolve, reject) => {
-    for (const type of item) {
-      console.log(type);
-    }
-  });
-}
