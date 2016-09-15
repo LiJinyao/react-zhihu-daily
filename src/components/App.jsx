8b325616e5042cbd063b1c30fde8fe271d0dac27
LiJinyao <lijinyao@outlook.com>
2016-09-15 21:52:28 +0800

    transition between router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import style from './App.styl';
 import Header from './Header';
 import Footer from './Footer';
-
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 // because Header have a Link linkto Index, but we need it actived **except** one path.
 // so we need let it know its location.
 
 export default (({ children, location }) => (
   <div className={style.root}>
     <Header location={location} />
-    {children}
+    <ReactCSSTransitionGroup
+      component="div"
+      transitionName="fade"
+      transitionEnterTimeout={600}
+      transitionLeaveTimeout={600}
+    >
+    {React.cloneElement(children, { key: location.pathname })}
+    </ReactCSSTransitionGroup>
+
     <Footer />
   </div>
 ));
